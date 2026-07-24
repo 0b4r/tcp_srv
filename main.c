88a37bf12d34f8c00a0f1200920783667ff7f4df
0b4r <0b4r@proton.me>
2026-07-24 10:40:47 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #define OUT_PORT 2324
-#define MSG_MAXLEN 1024
+#define MSG_MAXLEN 512
 
 int main(void)
 {
@@ -41,12 +42,13 @@ int main(void)
         exit(EXIT_FAILURE);
     }
     
-    printf("listening ..\n");
+    printf("listening...\n");
     
     int usr_fd;
     struct sockaddr_in usr_addr = {0};
     socklen_t usr_addr_len = sizeof(usr_addr);
     char usr_ip[INET_ADDRSTRLEN] = {0};
+
     while (1) {
         usr_fd = accept(srv_fd, (struct sockaddr *)&usr_addr, &usr_addr_len);
         
@@ -60,20 +62,31 @@ int main(void)
         printf("received connection from %s:%d\n", usr_ip, ntohs(usr_addr.sin_port));
         
         char usr_msg[MSG_MAXLEN] = {0};
+
         while (1) {
-            ssize_t msg_len = recv(usr_fd, usr_msg, sizeof(usr_msg), 0);
+            ssize_t n = recv(usr_fd, usr_msg, sizeof(usr_msg), 0);
             
-            if (msg_len == 0) {
+            if (n == 0) {
                 printf("client %s:%d disconnected\n", usr_ip, ntohs(usr_addr.sin_port));
                 break;
-            } else if (msg_len < 0) {
+            } else if (n < 0) {
                 perror("recv");
             } else {
-                
+                usr_msg[n] = '\0';
+                usr_msg[strcspn(usr_msg, "\r\n")] = '\0';
+
+
+                if (strcmp(usr_msg, "exit") == 0) {
+                    printf("%s:%d left\n", usr_ip, ntohs(usr_addr.sin_port));
+                    break;
+                }
+
+                printf("%s: %s\n", usr_ip, usr_msg);
+                memset(usr_msg, 0, sizeof(usr_msg));
             }
         }
+        close(usr_fd);
     }
-    close(usr_fd);
     close(srv_fd);
     
     return 0;
